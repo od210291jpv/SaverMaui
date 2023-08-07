715d8e17ccfb9a353dbe19db1e8fe123f19dfadf
Me me <od210291jpv@gmail.com>
2023-08-07 10:55:02 +0300

    IMplemented user and game card models

---
+++ BananasGamblerBackend/Database/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using BananasGamblerBackend.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BananasGamblerBackend.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<GameCard> GameCards { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(c => c.Cards)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId)
+                .IsRequired(false);
+        }
+    }
+}
---
+++ BananasGamblerBackend/Database/Models/GameCard.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BananasGamblerBackend.Database.Models
+{
+    public class GameCard
+    {
+        [ForeignKey(nameof(Id))]
+        public int Id { get; set; }
+
+        public string CardTitle { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        public int? UserId { get; set; }
+
+        public User? User { get; set; }
+    }
+}
