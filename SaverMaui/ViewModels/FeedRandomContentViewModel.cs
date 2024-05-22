716126450f32af3b6db5da04be22a1e139da36bc
Me me <od210291jpv@gmail.com>
2024-05-22 11:47:18 +0300

    Implemented ability to add random image into favorites

--- SaverMaui/ViewModels/FeedRandomContentViewModel.cs
+++ SaverMaui/ViewModels/FeedRandomContentViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Realms;
+using SaverMaui.Custom_Elements;
+using SaverMaui.Models;
 
 namespace SaverMaui.ViewModels
 {
     public class FeedRandomContentViewModel : BaseViewModel
     {
+        public static FeedRandomContentViewModel Instance { get; private set; }
+
+        private ImageRepresentationElement currentImage;
+
+        public ImageRepresentationElement CurrentImage
+        {
+            get { return currentImage; }
+            set
+            {
+                currentImage = value;
+                OnPropertyChanged("CurrentImage");
+            }
+        }
+
+        public FeedRandomContentViewModel()
+        {            
+            Instance = this;
+        }
     }
 }
--- SaverMaui/Views/RandomContentPage.xaml.cs
+++ SaverMaui/Views/RandomContentPage.xaml.cs
@@ -1,9 +1,48 @@
-﻿namespace SaverMaui.Views;
+﻿using CommunityToolkit.Maui.Core;
+using Realms;
+using SaverMaui.Custom_Elements;
+using SaverMaui.Models;
+using SaverMaui.ViewModels;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace SaverMaui.Views;
 
 public partial class RandomContentPage : ContentPage
 {
     public RandomContentPage()
     {
         InitializeComponent();
+        this.Appearing += OnPageAppearing;
+    }
+
+    private async void OnPageAppearing(object sender, EventArgs e)
+    {
+        Realm _realm = Realm.GetInstance();
+        var all = _realm.All<Content>().ToArray();
+
+        var randomContent = all[new Random().Next(0, all.Length - 1)];
+        if (FeedRandomContentViewModel.Instance != null) 
+        {
+            FeedRandomContentViewModel.Instance.CurrentImage = new ImageRepresentationElement()
+            {
+                CategoryId = randomContent.CategoryId ?? new Guid(),
+                Source = randomContent.ImageUri,
+                Name = randomContent.Title
+            };
+        }
+    }
+
+    private async void OnTapGestureRecognizerTapped(object sender, TappedEventArgs e)
+    {
+        Realm _realm = Realm.GetInstance();
+        var all = _realm.All<Content>().ToArray();
+
+        Content feed = all.Where(i => i.ImageUri.ToString().Contains(FeedRandomContentViewModel.Instance.CurrentImage.Source.ToString().Replace("Uri: ", ""))).FirstOrDefault();
+
+        _realm.Write(() => feed.IsFavorite = true);
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        var toast = Toast.Make($"Content added to favorites", ToastDuration.Short, 14);
+        await toast.Show(cancellationTokenSource.Token);
     }
 }
