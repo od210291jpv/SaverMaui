92c6f4d8f4be92c090f7370be5864438972971bc
Me me <od210291jpv@gmail.com>
2024-06-11 11:58:28 +0300

    updated configs

--- SaverMaui/Services/Helpers/UriHelper.cs
+++ SaverMaui/Services/Helpers/UriHelper.cs
@@ -9,8 +9,8 @@
         public static int Port = 80;
         public static int SignalRPort = 80;
 
-        public static string ImageRecognitionHost = "192.168.88.33";
-        public static string ImageRecognitionApi = "http://192.168.88.33/RecognizeImage?imageUri=";
+        public static string ImageRecognitionHost = "192.168.88.20";
+        public static string ImageRecognitionApi = "http://192.168.88.20/RecognizeImage?imageUri=";
 
         public static string SendNotificationUrl = $"http://{BaseIp}:{SignalRPort}/notify";
 
