6ded2b909cc04ef91d875e9b2e039e9f6c999785
pavlo.yutish <od210291jpv@gmail.com>
2025-08-20 15:38:48 +0300

    Implemented new screen (pre-alfa)

---
+++ SaverMaui/ViewModels/TiledFeedViewModel.cs
@@ -0,0 +1,37 @@
+﻿using SaverMaui.Custom_Elements;
+using System.Collections.ObjectModel;
+
+namespace SaverMaui.ViewModels
+{
+    public class TiledFeedViewModel : BaseViewModel
+    {
+        private ObservableCollection<ImageRepresentationElement> contentCollection;
+
+        public ObservableCollection<ImageRepresentationElement> ContentCollection
+        {
+            get => this.contentCollection;
+            set
+            {
+                this.contentCollection = value;
+                OnPropertyChanged(nameof(ContentCollection));
+            }
+        }
+
+        private ImageRepresentationElement currentImage;
+
+        public ImageRepresentationElement CurrentImage 
+        { 
+            get => currentImage;
+            set 
+            {
+                this.currentImage = value;
+                OnPropertyChanged(nameof(CurrentImage));
+            }
+        }
+
+        public TiledFeedViewModel()
+        {
+            this.ContentCollection = new ObservableCollection<ImageRepresentationElement>();
+        }
+    }
+}
