a187e5d4f680d0af9ec4c594e3eff6d9290a7a0a
pavlo.yutish <od210291jpv@gmail.com>
2026-03-11 22:02:50 +0200

    Removed logged client hardc depedency

---
+++ ImportAllContent/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Flurl.Http;
+using Microsoft.EntityFrameworkCore;
+using SaverBackend.Models;
+
+string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+Console.WriteLine("Hello, World!");
+
+DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+    .UseMySql(
+            "Server=192.168.88.252;Database=mobilesdb2;Uid=user;Pwd=password;",
+            ServerVersion.AutoDetect("Server=192.168.88.252;Database=mobilesdb2;Uid=user;Pwd=password;"))
+    .Options;
+
+ApplicationContext context = new ApplicationContext(options);
+
+var content = context.Contents.ToArray();
+
+
+Console.WriteLine(content.Length);
+
+int length = content.Length;
+int counter = 0;
+
+foreach (var cont in content) 
+{
+    await cont.ImageUri.DownloadFileAsync($"{directory}/data");
+    counter++;
+    Console.WriteLine($"Downloaded {counter} of {length}");
+}
