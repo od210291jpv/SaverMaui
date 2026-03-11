7779809f37a46c9f624e1d3216096b08d026a234
od210291jpv <od210291jpv@gmail.com>
2026-03-11 22:42:14 +0200

    Solution cleanup (#38)
    * Cleaup
    
    * Removed logged client hardc depedency

--- LiveHost/DataStructures/RabbitMqMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LiveHost.DataStructures
-{
-    public class RabbitMqMessage
-    {
-        public string Topic { get; set; } = string.Empty;
-
-        public object[] Data { get; set; } = Array.Empty<object>();
-    }
-}
